234bea5f23ec0bc54cb301aa4f5432e6198ddee0
vimlinuz <vimlinuz@gmail.com>
2026-06-06 21:13:01 +0545

    config: basic config workaround

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A Nix-flake-based ZenLoadBar development environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+  };
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.nodejs
+          pkgs.pnpm
+          pkgs.prettier
+          pkgs.typescript-language-server
+        ];
+      };
+    };
+}
