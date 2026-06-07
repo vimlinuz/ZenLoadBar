52766556cf4ffb13cab08fc332cfac28f8dbbd81
vimlinuz <vimlinuz@gmail.com>
2026-06-07 21:13:52 +0545

    link: linked the js and css files and set the start up to document_start

--- src/background.js
+++ src/background.js
@@ -1,37 +1,8 @@
-(async () => {
-  const [tab] = await browser.tabs.query({
-    active: true,
-    currentWindow: true,
-  });
-  while (true) {
-    if (tab.status === "complete") {
-      await browser.tabs.sendMessage(tab.id, { command: "reset" });
-      document.head.removeChild(style);
-      document.head.removeChild(line);
-      break;
-    }
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
+  if (changeInfo.status === "loading") {
+    browser.tabs.sendMessage(tabId, { command: "show" }).catch(() => {});
   }
-})();
-// async function getCurrentTab() {
-//   return browser.tabs.getCurrent();
-// }
-//
-// (async () => {
-//   while (true) {
-//     const currentTab = await getCurrentTab();
-//     if (currentTab.status === "complete") {
-//     }
-//   }
-// })();
-//
-// // browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-// //   browser.tabs.sendMessage(tabs[0].id, { action: "removeElements" });
-// // });
-// function doTabLoadedStuff(tabId, changeInfo, tabInfo) {
-//   if (changeInfo.status != "complete") return;
-//   console.log("Page loaded in tab " + tabId);
-// }
-// browser.tabs.onUpdated.addListener(doTabLoadedStuff, {
-//   urls: ["https://vimlinuz.github.io/mdecho"],
-//   properties: ["status"],
-// });
+  if (changeInfo.status === "complete") {
+    browser.tabs.sendMessage(tabId, { command: "hide" }).catch(() => {});
+  }
+});
--- src/main.js
+++ src/main.js
@@ -1,50 +1,28 @@
-// const line = document.createElement("div");
-//
-// Object.assign(line.style, {
-//   position: "fixed",
-//   top: "0",
-//   // (100-10)/2
-//   left: "45%",
-//   width: "10%",
-//   height: "2px",
-//   background: "white",
-//   zIndex: "999999",
-// });
-//
-// document.body.appendChild(line);
-
 const line = document.createElement("div");
-
 Object.assign(line.style, {
   position: "fixed",
   top: "0",
   left: "45%",
-  // width: "10%",
+  width: "4%",
   height: "2px",
   background: "white",
   zIndex: "999999",
   animation: "loading 1s linear infinite",
 });
 
-const style = document.createElement("style");
-style.textContent = `
-  @keyframes loading {
-    from {
-      width: 0%;
-    }
-    to {
-      width: 10%;
-    }
+function show() {
+  if (!line.isConnected) {
+    document.documentElement.appendChild(line);
   }
-`;
-
-document.head.appendChild(style);
-document.body.appendChild(line);
-
-async function reset(tab) {
-  await browser.scripting.removeCSS({
-    target: { tabId: tab.id },
-    css: hidePage,
-  });
-  await browser.tabs.sendMessage(tab.id, { command: "reset" });
 }
+
+function hide() {
+  if (line.isConnected) line.remove();
+}
+
+show();
+
+browser.runtime.onMessage.addListener((message) => {
+  if (message.command === "show") show();
+  if (message.command === "hide") hide();
+});
